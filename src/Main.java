64a24f6b4dae64258667cb53a60a14fb44524221
Mor <toxice290@gmail.com>
2025-11-23 22:22:09 +0200

    Enough For Tonight 23.11.25

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import biuoop.DrawSurface;
+import biuoop.GUI;
+
+import java.awt.*;
+
+public class Main {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static final String Title = "Assignment 2 - Arkanoid";
+    static void main() {
+        GUI gui = new GUI(Title, WIDTH, HEIGHT);
+        }
+}
